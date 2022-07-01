b2f6474ac7679e052662b4c08baf8bab722c49fe
tubi55 <tubi55@nate.com>
2022-07-01 19:32:21 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -7,11 +7,30 @@ import Txt from './components/Txt';
 import ColorPicker from './components/ColorPicker';
 import Dragable from './components/Dragable';
 
-import { useState } from 'react';
-import { Canvas, extend, useThree } from 'react-three-fiber';
-import { Suspense } from 'react';
+import { Canvas, extend, useFrame } from 'react-three-fiber';
+import { Suspense, useRef, useState } from 'react';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 extend({ OrbitControls });
+
+function Box() {
+	const obj = useRef(null);
+	useFrame((state) => {
+		obj.current.rotation.x += 0.03;
+		obj.current.rotation.y += 0.03;
+	});
+	return (
+		<mesh ref={obj}>
+			<boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+			<meshStandardMaterial
+				attach='material'
+				transparent
+				opacity={0.5}
+				wireframe
+				color={'#000'}
+			/>
+		</mesh>
+	);
+}
 
 function App() {
 	const [Index, setIndex] = useState(0);
@@ -33,7 +52,7 @@ function App() {
 				<ambientLight color='white' intensity={1} />
 
 				{/* <Dragable transformGroup> */}
-				<Suspense fallback={null}>
+				<Suspense fallback={<Box />}>
 					<Model
 						path={process.env.PUBLIC_URL + '/car/scene.gltf'}
 						position={[0, -1, 4]}
